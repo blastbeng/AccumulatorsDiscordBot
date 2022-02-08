26949c659a688cb9745c420b7de42ddc05aba7fc
rodfval.fv@gmail.com <2Fa74lov$>
2022-02-08 14:23:52 +0100

    removed log

--- index.js
+++ index.js
@@ -30,8 +30,6 @@ client.on("messageCreate", function (message) {
         async function lsWithGrep() {
             try {
                 const { stdout, stderr } = await exec("/usr/bin/curl -X 'GET' 'http://192.168.1.160:5500/api/tts?voice=marytts%3Aistc-lucia-hsmm&text=andate%20a%20fanculo%20stronzi%20luridi%20accumulatori&vocoder=high&denoiserStrength=0.03&cache=false'   -H 'accept: */*' --output /ramdisk/prova.wav");
-                console.log('stdout:', stdout);
-                console.log('stderr:', stderr);
                 const connection = joinVoiceChannel({
                     channelId: message.member.voice.channel.id,
                     guildId: message.guild.id,
