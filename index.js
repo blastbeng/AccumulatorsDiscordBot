6701df99d5116b4ccbdb1327f1ed831472a43c98
rodfval.fv@gmail.com <2Fa74lov$>
2022-02-08 12:01:35 +0100

    prova

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const https = require('http');
 const fs = require('fs');
 const config = require("./config.json");
 const { joinVoiceChannel } = require('@discordjs/voice');
+const { createAudioPlayer } = require('@discordjs/voice');
+const { createAudioResource } = require('@discordjs/voice');
 const querystring = require("querystring");
 const { Curl } = require("node-libcurl");
+const player = createAudioPlayer();
 
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES"]
 });
 
 const prefix = "!";
-const dir = "/ramdisk";
 
 
 client.on("messageCreate", function (message) {
@@ -31,31 +33,27 @@ client.on("messageCreate", function (message) {
         curl.setOpt(Curl.option.HTTPGET, true);
         curl.on("end", function (statusCode, data, headers) {
 
-            let server = message.guild.id;
-
-
-            const connection = joinVoiceChannel({
-                channelId: message.member.voice.channel.id,
-                guildId: message.guild.id,
-                adapterCreator: message.guild.voiceAdapterCreator
-            });
-            //connection.on(joinVoiceChannel.Ready, () => {
-
-            fs.writeFile(dir+"/prova.wav", data, (err) => {
+            fs.writeFile("/ramdisk/prova.wav", data, (err) => {
                     if (err)
                         console.log(err);
                     else {
-                        const dispatcher = connection.playFile(dir+"/prova.wav");
-                        dispatcher.on("end", end => {
-                            VC.leave()
+                        const connection = joinVoiceChannel({
+                            channelId: message.member.voice.channel.id,
+                            guildId: message.guild.id,
+                            adapterCreator: message.guild.voiceAdapterCreator
                         });
+                        const resource = createAudioResource("/ramdisk/prova.wav");
+                        player.play(resource);
+                        connection.subscribe(player);
+                        player.stop();
+                        
+                        connection.destroy();
                     }
                 });
 
             //console.log('The connection has entered the Ready state - ready to play audio!');
             //});
 
-            connection.destroy();
             this.close();
         });
         curl.on("error", terminate);
